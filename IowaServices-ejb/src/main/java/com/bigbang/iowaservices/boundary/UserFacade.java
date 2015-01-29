6173ef4b13b2af75f65d6b638341fd3c12ac3c29
Suraj Byanju <surajbyanju47@gmail.com>
2015-01-28 20:14:12 -0600

    added datasources

---
+++ IowaServices-ejb/src/main/java/com/bigbang/iowaservices/boundary/UserFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bigbang.iowaservices.boundary;
+
+import com.bigbang.iowaservices.entities.User;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author dell
+ */
+@Stateless
+public class UserFacade extends AbstractFacade<User> implements UserFacadeLocal {
+    @PersistenceContext(unitName = "IOWA_SERVICE")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UserFacade() {
+        super(User.class);
+    }
+    
+}
--- IowaServices-ejb/src/main/java/com/bigbang/iowaservices/entities/User.java
+++ IowaServices-ejb/src/main/java/com/bigbang/iowaservices/entities/User.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ea.project.iowaservices.entities;
+package com.bigbang.iowaservices.entities;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
