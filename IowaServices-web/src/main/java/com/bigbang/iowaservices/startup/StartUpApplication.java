7892908f649f341e48e1736982e120e122c3c916
Suraj Byanju <surajbyanju47@gmail.com>
2015-02-16 14:17:28 -0600

    startup application created

---
+++ IowaServices-web/src/main/java/com/bigbang/iowaservices/startup/StartUpApplication.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bigbang.iowaservices.startup;
+
+import com.bigbang.iowaservices.boundary.SkillFacadeLocal;
+import com.bigbang.iowaservices.boundary.UsersFacadeLocal;
+import com.bigbang.iowaservices.entities.Skill;
+import com.bigbang.iowaservices.entities.Users;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ *
+ * @author dell
+ */
+@Singleton
+@Startup
+public class StartUpApplication {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    @EJB 
+    UsersFacadeLocal userFacade;
+    @EJB
+    SkillFacadeLocal SkillFacade;
+    Users user;
+    Skill skill;
+    
+    @PostConstruct
+    public void initApp(){
+    
+        user = new Users();
+        
+        user.setPassword("admin");
+        user.setRole("ROLE_ADMIN");
+        user.setUsername("admin");
+        user.setEnabled(Boolean.TRUE);
+        userFacade.create(user);
+        
+        skill = new Skill();
+        skill.setCode("CAR");
+        skill.setDescription("Carpenting");
+        skill.setName("Carpening");
+        SkillFacade.create(skill);
+        
+        skill = new Skill();
+        skill.setCode("PAINT");
+        skill.setDescription("Painting");
+        skill.setName("Painting");
+        SkillFacade.create(skill);
+        
+        skill = new Skill();
+        skill.setCode("CARWASH");
+        skill.setDescription("Car Washing");
+        skill.setName("Car Washing");
+        SkillFacade.create(skill);
+        
+        skill = new Skill();
+        skill.setCode("FUR");
+        skill.setDescription("Furnishing");
+        skill.setName("Furnishing");
+        SkillFacade.create(skill);
+    }
+}
